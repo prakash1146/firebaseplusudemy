aca76a520f553b8ceacd7b5f38a833ca6fa18fc0
Prakash Chandra Kesarwani <p.chandra.kesarwani@accenture.com>
2019-08-29 19:09:46 +0530

    first checkin

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCEK8M8-48nGpaIAvOUnAzJSDgi-IXr5GI",
+    authDomain: "udemyprojectpractice.firebaseapp.com",
+    databaseURL: "https://udemyprojectpractice.firebaseio.com",
+    projectId: "udemyprojectpractice",
+    storageBucket: "",
+    messagingSenderId: "561778273845",
+    appId: "1:561778273845:web:08c4da5c0f24e9db"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCEK8M8-48nGpaIAvOUnAzJSDgi-IXr5GI",
+    authDomain: "udemyprojectpractice.firebaseapp.com",
+    databaseURL: "https://udemyprojectpractice.firebaseio.com",
+    projectId: "udemyprojectpractice",
+    storageBucket: "",
+    messagingSenderId: "561778273845",
+    appId: "1:561778273845:web:08c4da5c0f24e9db"
+  }
 };
 
 /*
